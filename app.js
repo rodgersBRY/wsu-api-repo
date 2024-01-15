281c96b17b9bb86dfc529f531d02c516c2d44e35
Brian Mawira <brian.mawi@yahoo.com>
2024-01-15 13:50:31 +0300

    connect to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express"),
+  { MongoClient } = require("mongodb"),
   port = 3000,
   app = express();
+
+// uri to local instance
+const uri = "mongodb://localhost:27017/healthDB";
+
+const client = new MongoClient(uri);
+
+// connect to mongodb
+client
+  .connect()
+  .then(() => {
+    console.log("connected to the db");
+  })
+  .catch((err) => {
+    console.error(`Error connecting to db: ${err}`);
+  });
 
 // Middleware function to log timestamp and requested endpoint
 const requestLoggerMiddleware = (req, res, next) => {
