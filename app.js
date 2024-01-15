8855bf4f58adb6e237589a5ece1ec59106dcc9d3
Brian Mawira <brian.mawi@yahoo.com>
2024-01-15 12:35:08 +0300

    create /api/health endpoint

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const port = 3000;
 
 const app = express();
 
+app.use("/api/health", (req, res, next) => {
+  res.json({ message: "Server is healthy" });
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
