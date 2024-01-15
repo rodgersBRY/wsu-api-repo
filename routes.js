97a5dcdc76735fa197eed08827cebd869bc836fd
Brian Mawira <brian.mawi@yahoo.com>
2024-01-15 15:34:00 +0300

    comment code

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const app = require("express").Router();
 const userController = require("./user.controller");
 
+// define the get, post, put and delete requests against routes
 app.route("/").get(userController.getUsers).post(userController.addUser);
 app
   .route("/:id")
